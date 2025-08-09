f358adb5cdeed02528247c5aa112b6c2a70e8666
Esteban-Rosas <70412769+Esteban-Rosas@users.noreply.github.com>
2025-08-08 20:09:43 -0500

    Update conexion.php

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,7 +1,7 @@
 <?php
 // includes/conexion.php
 
-$host = 'dpg-d2ah3gjuibrs73ael9vg-a';     // o IP de tu servidor
+$host = 'dpg-d2ah3gjuibrs73ael9vg-a.oregon-postgres.render.com';     // o IP de tu servidor
 $port = '5432';          // Puerto por defecto de PostgreSQL
 $dbname = 'smaq';        // Nombre de tu base de datos
 $user = 'smaq_user';    // Reemplaza con tu usuario PostgreSQL
